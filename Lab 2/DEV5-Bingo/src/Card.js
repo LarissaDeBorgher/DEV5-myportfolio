8461db570e7925b926f3a2a951cc1bda51634857
LarissaDeBorgher <r0839981@student.thomasmore.be>
2022-10-06 16:19:31 +0200

    feat: mark cards as done

--- Lab 2/DEV5-Bingo/src/Card.js
+++ Lab 2/DEV5-Bingo/src/Card.js
@@ -13,6 +13,7 @@ export default class Card {
     console.log("Marking card as done");
     console.log(target);
     // hint: use class .bingo__card--done
+    target.classList.toggle("bingo__card--done");
   }
 
   render(counter) {
@@ -31,9 +32,11 @@ export default class Card {
 
     // 🔥🔥🔥 TODO4: when we click an item, we want to check for winners and we want to save the selection to storage
     card.addEventListener("click", (e) => {
-      // this.markDone(e.target);
+      this.markDone(e.target);
       // call checkWinner() on the Bingo class
       // try to call the save() method on the Bingo class
+      Bingo.checkWinner();
+      Bingo.save();
     });
   }
 }
